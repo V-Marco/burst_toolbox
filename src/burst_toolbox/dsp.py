a3c83b3254ec5e1c871511c83aa35d2b79536c21
V-Marco <vsomelyusik@gmail.com>
2026-08-17 14:58:43 -0500

    update spectrogram_multitaper func

--- src/burst_toolbox/dsp.py
+++ src/burst_toolbox/dsp.py
@@ -1,20 +1,21 @@
 import numpy as np
 from scipy import signal
 import tqdm
+from mne.time_frequency import tfr_array_multitaper
 
 def compute_amplitude(LFP: np.ndarray, freq_band: np.ndarray, fs: int = 1000) -> np.ndarray:
     '''
     Parameters:
-    ----------
+    -----------
     LFP: np.ndarray of shape (n_trials, time)
         LFP matrix.
     freq_band: np.ndarray of shape (2,)
         Frequency band to filter LFPs in.
     fs: int, default = 1000
-        Sampling frequency.
+        Sampling rate.
     
     Returns:
-    ----------
+    --------
     out: np.ndarray of shape (n_trials, time)
         Computed analytical amplitude.
     '''
@@ -28,101 +29,58 @@ def compute_amplitude(LFP: np.ndarray, freq_band: np.ndarray, fs: int = 1000) ->
 def compute_power(LFP: np.ndarray, freq_band: np.ndarray, fs: int = 1000):
     '''
     Parameters:
-    ----------
+    -----------
     LFP: np.ndarray of shape (n_trials, time)
         LFP matrix.
     freq_band: np.ndarray of shape (2,)
         Frequency band to filter LFPs in.
     fs: int, default = 1000
-        Sampling frequency.
+        Sampling rate.
     
     Returns:
-    ----------
+    --------
     out: np.ndarray of shape (n_trials, time)
         Computed power.
     '''
     return compute_amplitude(LFP, freq_band, fs) ** 2
 
-def spectrogram_multitaper(LFP: np.ndarray, freqs: np.ndarray = np.arange(1, 150), n_cycles: int = 7, freq_smoothing: float = 0.25, fs: int = 1000):
+def spectrogram_multitaper(LFP: np.ndarray, freqs: np.ndarray, correct_1f: bool = True, fs: int = 1000):
     '''
-    Compute a spectrogram using the multitaper approach.
+    Make a spectrogram using the multitaper approach.
+    https://mne.tools/stable/generated/mne.time_frequency.tfr_array_multitaper.html#mne.time_frequency.tfr_array_multitaper
 
     Parameters:
     -----------
     LFP: np.ndarray of shape (n_trials, time)
         LFP matrix.
-    
-    freqs: np.ndarray of shape (n_freqs,), default = np.arange(1, 150)
-        Frequencies over which to compute the spectrogram.
-
-    n_cycles: int, default = 7
-        Number of cycles at each frequency. Window length is computed as L = n_cycles / frequency.
-
-    freq_smoothing: float, default = 0.25
-        Smoothing as a proportion of each frequency. NW = ((freq_smoothing * freq) * win_length / fs) / 2.0
-
+    freqs: np.ndarray of shape (n_freqs, )
+        Frequencies in Hz.
+    correct_1f: bool, default = True
+        Whether to apply 1/f correction.
     fs: int, default = 1000
-        Sampling frequency.
-    
+        Sampling rate.
+
     Returns:
-    ----------
-    t: np.ndarray of shape (time,)
-        Time vector.
+    --------
+    Pxx: np.ndarray of shape (n_trials, n_freqs, time)
+        Spectrogram [dB].
 
-    freqs: np.ndarray of shape (n_freqs,)
-        Same as input freqs.
-
-    power: np.ndarray of shape (n_trials, n_freqs, T)
-        Computed spectrogram.
     '''
-
-    N, T = LFP.shape
-    t = np.arange(T) / fs
-    power = np.zeros((N, len(freqs), T))
-
-    for freq_idx, freq in tqdm.tqdm(enumerate(freqs), total = len(freqs)):
-
-        # Set a frequency-dependent window length: L = n_cycles / freq
-        win_length = int(np.round((n_cycles / freq) * fs))
-        if win_length < 3: continue
-
-        # Ensure odd length for symmetric centering
-        if win_length % 2 == 0:
-            win_length += 1
-
-        # Frequency smoothing: df = freq_smoothing * freq
-        df = freq_smoothing * freq
-        NW = (df * win_length / fs) / 2.0
-
-        Kmax = int(np.floor(2 * NW))
-        if Kmax < 1: continue
-
-        tapers = signal.windows.dpss(win_length, NW, Kmax)  # (K, win_length)
-
-        # Precompute FFT freqs for this window length
-        freqs_fft = np.fft.rfftfreq(win_length, 1 / fs)
-        idx = np.argmin(np.abs(freqs_fft - freq))
-
-        # Sliding-window multitaper power
-        for trial_idx in range(N):
-            for ti in range(T):
-                win_start = ti - win_length // 2
-                win_end = win_start + win_length
-                
-                # Pad with 0s
-                seg = np.zeros(win_length)
-                seg_start = max(0, win_start)
-                seg_end   = min(T, win_end)
-
-                # Where to place the real data inside the padded window
-                pad_start = seg_start - win_start
-                pad_end   = pad_start + (seg_end - seg_start)
-
-                seg[pad_start:pad_end] = LFP[trial_idx, seg_start:seg_end]
     
-                # Broadcast: (K, win_length) * (1, win_length)
-                tapered = tapers * seg[None, :]
-                fft_vals = np.fft.rfft(tapered, axis=1)
-                power[trial_idx, freq_idx, ti] = np.mean(np.abs(fft_vals[:, idx])**2)
+    Pxx = tfr_array_multitaper(
+        data = LFP.reshape((LFP.shape[0], 1, LFP.shape[1])), 
+        sfreq = fs, 
+        freqs = freqs, 
+        n_cycles = freqs / 4, 
+        time_bandwidth = 3.0, 
+        output = "power").squeeze() # (n_trials, n_freqs, time)
+    
+    Pxx = Pxx + 1e-15
+    
+    # 1/f correct
+    # -----------
+    if correct_1f:
+        b, a = np.polyfit(np.log10(freqs), np.nanmean(Pxx, axis = (2, 0)), 1)
+        Pxx = Pxx - (a + b * np.log10(freqs))[:, None]
 
-    return t, freqs, power
+    return 10 * Pxx
